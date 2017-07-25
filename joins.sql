a94ace844037119abb3b5e5f0d1fd37d0977cac9
Reyn-L <Reyn.Leong@gmail.com>
2017-07-24 17:19:09 -1000

    query 4

--- joins.sql
+++ joins.sql
@@ -1,8 +1,17 @@
-SELECT *
-FROM users;
+-- SELECT *
+-- FROM users;
 
-SELECT *
+-- SELECT *
+-- FROM posts p
+-- INNER JOIN users u
+-- ON p.users_id = 100;
+
+-- SELECT p.id, p.title, p.url, p.content, p.created_at, p.updated_at, u.first_name, u.last_name
+-- FROM posts p
+-- INNER JOIN users u
+-- ON p.users_id = 200;
+
+SELECT p.id, p.title, p.url, p.content, p.created_at, p.updated_at, u.username
 FROM posts p
 INNER JOIN users u
-ON p.users_id = 100;
-
+ON u.first_name = 'Norene' AND u.last_name = 'Schmitt'
